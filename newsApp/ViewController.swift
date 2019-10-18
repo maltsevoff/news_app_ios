a8da2c0d1364e99ed0c97c20e1b9b91c7b684551
maltsevoff <alexmalczev@gmail.com>
2019-10-18 12:57:04 +0300

    init

---
+++ newsApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Aleksandr Maltsev on 18.10.2019.
+//  Copyright © 2019 Aleksandr Maltsev. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
